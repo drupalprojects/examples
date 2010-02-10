625891d46dea8f32294127a55a46d8b229c019d8
Dave Reid <dave@davereid.net>
2010-02-10 21:46:26 +0000

    by Dave Reid: Fixed line endings and other misc improvements.

--- vertical_tabs_example/vertical_tabs_example.js
+++ vertical_tabs_example/vertical_tabs_example.js
@@ -1,16 +1,18 @@
 // $Id$
 
+// The following line will prevent a JavaScript error if this file is included
+// and vertical tabs are disabled.
 Drupal.verticalTabs = Drupal.verticalTabs || {};
 
 Drupal.verticalTabs.vertical_tabs_example = function() {
-  if ($('#edit-vertical_tabs_example-enabled').attr('checked')) {
-    // We show here the container with the custom settings form
-    $('#vertical_tabs_example-custom-container').show('fast')
-    return $('#edit-vertical_tabs_example-custom-setting').val();
+  if ($('#edit-vertical-tabs-example-enabled').attr('checked')) {
+    // We show here the container with the custom settings form.
+    $('#edit-vertical-tabs-example-custom-setting-wrapper').parent().show('fast')
+    return Drupal.checkPlain($('#edit-vertical-tabs-example-custom-setting').val());
   }
   else {
-    // We hide here the container with the custom settings form
-    $('#vertical_tabs_example-custom-container').hide('fast')
-    return Drupal.t('Using default.');
+    // We hide here the container with the custom settings form.
+    $('#edit-vertical-tabs-example-custom-setting-wrapper').parent().hide('fast')
+    return Drupal.t('Using default');
   }
 }
