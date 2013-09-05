f7e8df2d9f1c0b8a45abacfddaa636075eb0c9eb
Mile23 <Mile23@116231.no-reply.drupal.org>
2013-09-04 20:21:18 -0700

    Issue #2079685 by Mile23: Add SimpleTest for base Examples module.

---
+++ lib/Drupal/examples/Tests/ExamplesTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file
+ * SimpleTests for examples module.
+ */
+
+namespace Drupal\examples\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Minimal test case for the examples module.
+ *
+ * @ingroup examples
+ */
+class ExamplesTest extends WebTestBase {
+
+  /**
+   * Modules to install.
+   *
+   * @var array
+   */
+  public static $modules = array('examples');
+
+  /**
+   * Implements getInfo().
+   *
+   * @return array
+   *   Testing info for SimpleTest.
+   */
+  public static function getInfo() {
+    return array(
+      'name' => 'Examples test',
+      'description' => 'Functional tests for the Examples module',
+      'group' => 'Examples',
+    );
+  }
+
+  /**
+   * Test whether the module was installed.
+   *
+   * @todo: Replace module_exists with Drupal::moduleHandler() ..etc.
+   */
+  public function testExamples() {
+    //$this->assertTrue(Drupal::moduleHandler()->moduleExists('examples'));
+    $this->assertTrue(module_exists('examples'));
+  }
+
+}
