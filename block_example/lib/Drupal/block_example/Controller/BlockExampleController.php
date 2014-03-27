701bf9e920866af1096af9b740bc450296e0e063
milodesc <milodesc@1498524.no-reply.drupal.org>
2014-03-27 12:42:58 -0700

    Issue #2226927 by milodesc: Block example provides 3, not 2, sample blocks .

--- block_example/lib/Drupal/block_example/Controller/BlockExampleController.php
+++ block_example/lib/Drupal/block_example/Controller/BlockExampleController.php
@@ -17,7 +17,7 @@ class BlockExampleController {
    */
   public function description() {
     $build = array(
-      '#markup' => t('The Block Example provides two sample blocks which demonstrate the various block APIs. To experiment with the blocks, enable and configure them on <a href="@url">the block admin page</a>.', array('@url' => url('admin/structure/block'))),
+      '#markup' => t('The Block Example provides three sample blocks which demonstrate the various block APIs. To experiment with the blocks, enable and configure them on <a href="@url">the block admin page</a>.', array('@url' => url('admin/structure/block'))),
     );
 
     return $build;
