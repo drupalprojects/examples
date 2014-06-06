a221c4ff3e6a00f1c9dc3c06d26db09be358055d
git <git@1514702.no-reply.drupal.org>
2014-06-06 13:16:59 -0500

    Issue #2276929 by Mile23, piestep: Update PHPUnit Example to use *.menu_links.yml, add tests.

--- phpunit_example/src/Controller/PHPUnitExampleController.php
+++ phpunit_example/src/Controller/PHPUnitExampleController.php
@@ -30,7 +30,9 @@ class PHPUnitExampleController {
 <li><p>You can run PHPUnit-based tests from within Drupal 8 by enabling the Testing module and then selecting the PHPUnit group from the testing page. As of this writing, this method doesn&#39;t provide any useful output.</p></li>
 </ul>
 <h3>Standard PHPUnit Practices</h3>
-<p>You can (and really, should) run PHPUnit from the command line. On unix-based systems this means you need to <code>cd core</code> and then <code>./vendor/bin/phpunit</code>.</p>
+<p>You can (and really, should) run PHPUnit from the command line.</p>
+<p>On unix-based systems this means you need to <code>cd core</code> and then <code>./vendor/bin/phpunit</code>.</p>
+<p>On Windows-based systems, assuming you have php in your path, <code>php ./vendor/phpunit/phpunit/composer/bin/phpunit --group phpunit_example</code>.</p>
 <p>Also, you should mark your tests as belonging to a group, so they can be run independently. You do this by annotating your test classes with <code>@group GroupName</code>. Currently, no conventions exist for how these groups should be named, but your module machine name is a likely candidate. You use groups like this: <code>./vendor/bin/phpunit --group GroupName</code>.</p>
 <p>So, for instance, to run all of the PHPUnit example tests, you would type <code>./vendor/bin/phpunit --group phpunit_example</code>.</p>
 <p>As you can see, including a <code>@group</code> annotation is a good idea.</p>'),
