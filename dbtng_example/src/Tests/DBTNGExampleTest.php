eb67435c8d9bc29e5f3b2d1c24b7be3beff8f07e
git <git@1514702.no-reply.drupal.org>
2014-06-06 13:57:11 -0500

    Issue #2277359 by piestep, Mile23: Update DBTNG Example to test *.menu_links.yml.

--- dbtng_example/src/Tests/DBTNGExampleTest.php
+++ dbtng_example/src/Tests/DBTNGExampleTest.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @file
  * Contains \Drupal\dbtng_example\Tests\DBTNGExampleTest
@@ -15,26 +14,76 @@ use Drupal\simpletest\WebTestBase;
  */
 class DBTNGExampleTest extends WebTestBase {
 
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
   public static $modules = array('dbtng_example');
+
+  /**
+   * The installation profile to use with this test.
+   *
+   * We need the 'minimal' profile in order to make sure the Tool block is
+   * available.
+   *
+   * @var string
+   */
+  protected $profile = 'minimal';
 
   /**
    * {@inheritdoc}
    */
   public static function getInfo() {
     return array(
-      'name' => 'DBTNG example API and UI tests',
-      'description' => 'Various API tests on the dbtng example module.',
+      'name' => 'DBTNG example tests',
+      'description' => 'Various tests on the dbtng example module.',
       'group' => 'Examples',
     );
   }
 
   /**
-   * Assert that two entries were inserted at install.
+   * Regression test for dbtng_example.
+   *
+   * We'll verify the following:
+   * - Assert that two entries were inserted at install.
+   * - Test the example description page.
+   * - Verify that the example pages have links in the Tools menu.
    */
-  public function testInstall() {
+  public function testDBTNGExample() {
+    // Assert that two entries were inserted at install.
     $result = DBTNGExampleStorage::load();
     $this->assertEqual(
-        count($result), 2, 'Found two entries in the table after installing the module.'
+      count($result), 2, 'Found two entries in the table after installing the module.'
+    );
+
+    // Test the example description page.
+    $this->drupalGet('examples/dbtng_example');
+    $this->assertResponse(200, 'Description page exists.');
+
+    // Verify and validate that default menu links were loaded for this module.
+    $links = $this->providerMenuLinks();
+    foreach ($links as $page => $path) {
+      $this->drupalGet($page);
+      $this->assertLinkByHref($path);
+    }
+  }
+
+  /**
+   * Data provider for testing menu links.
+   *
+   * @return array
+   *   Array of page -> link relationships to check for:
+   *   - The key is the path to the page where our link should appear.
+   *   - The value is the link that should appear on that page.
+   */
+  protected function providerMenuLinks() {
+    return array(
+      '/' => 'examples/dbtng_example',
+      'examples/dbtng_example' => 'examples/dbtng_example/list',
+      'examples/dbtng_example' => 'examples/dbtng_example/add',
+      'examples/dbtng_example' => 'examples/dbtng_example/update',
+      'examples/dbtng_example' => 'examples/dbtng_example/advanced',
     );
   }
 
@@ -43,13 +92,13 @@ class DBTNGExampleTest extends WebTestBase {
    */
   public function testUI() {
     // Test the basic list.
-    $this->drupalGet('examples/dbtng');
+    $this->drupalGet('examples/dbtng_example');
     $this->assertPattern("/John[td\/<>\w\s]+Doe/", "Text 'John Doe' found in table");
 
     // Test the add tab.
     // Add the new entry.
     $this->drupalPostForm(
-      'examples/dbtng/add',
+      'examples/dbtng_example/add',
       array(
         'name' => 'Some',
         'surname' => 'Anonymous',
@@ -58,25 +107,25 @@ class DBTNGExampleTest extends WebTestBase {
       t('Add')
     );
     // Now find the new entry.
-    $this->drupalGet('examples/dbtng');
+    $this->drupalGet('examples/dbtng_example');
     $this->assertPattern("/Some[td\/<>\w\s]+Anonymous/", "Text 'Some Anonymous' found in table");
     // Try the update tab.
     // Find out the pid of our "anonymous" guy.
     $result = DBTNGExampleStorage::load(array('surname' => 'Anonymous'));
-    $this->drupalGet("examples/dbtng");
+    $this->drupalGet("examples/dbtng_example");
     $this->assertEqual(
-        count($result), 1, 'Found one entry in the table with surname = "Anonymous".'
+      count($result), 1, 'Found one entry in the table with surname = "Anonymous".'
     );
     $entry = $result[0];
     unset($entry->uid);
     $entry->name = 'NewFirstName';
-    $this->drupalPostForm('examples/dbtng/update', (array) $entry, t('Update'));
+    $this->drupalPostForm('examples/dbtng_example/update', (array) $entry, t('Update'));
     // Now find the new entry.
-    $this->drupalGet('examples/dbtng');
+    $this->drupalGet('examples/dbtng_example');
     $this->assertPattern("/NewFirstName[td\/<>\w\s]+Anonymous/", "Text 'NewFirstName Anonymous' found in table");
 
     // Try the advanced tab.
-    $this->drupalGet('examples/dbtng/advanced');
+    $this->drupalGet('examples/dbtng_example/advanced');
     $rows = $this->xpath("//*[@id='dbtng-example-advanced-list'][1]/tbody/tr");
     $this->assertEqual(count($rows), 1, 'One row found in advanced view');
     $this->assertFieldByXPath("//*[@id='dbtng-example-advanced-list'][1]/tbody/tr/td[4]", "Roe", "Name 'Roe' Exists in advanced list");
@@ -105,19 +154,19 @@ class DBTNGExampleTest extends WebTestBase {
     // Verify that 4 records are found in the database.
     $result = DBTNGExampleStorage::load();
     $this->assertEqual(
-        count($result), 4, 'Found a total of four entries in the table after creating two additional entries.'
+      count($result), 4, 'Found a total of four entries in the table after creating two additional entries.'
     );
 
     // Verify 2 of these records have 'Doe' as surname.
     $result = DBTNGExampleStorage::load(array('surname' => 'Doe'));
     $this->assertEqual(
-        count($result), 2, 'Found two entries in the table with surname = "Doe".'
+      count($result), 2, 'Found two entries in the table with surname = "Doe".'
     );
 
     // Now find our not-Doe entry.
     $result = DBTNGExampleStorage::load(array('surname' => 'NotDoe'));
     $this->assertEqual(
-        count($result), 1, 'Found one entry in the table with surname "NotDoe'
+      count($result), 1, 'Found one entry in the table with surname "NotDoe'
     );
     // Our NotDoe will be changed to "NowDoe".
     $entry = $result[0];
@@ -127,12 +176,12 @@ class DBTNGExampleTest extends WebTestBase {
 
     $result = DBTNGExampleStorage::load(array('surname' => 'NowDoe'));
     $this->assertEqual(
-        count($result), 1, "Found renamed 'NowDoe' surname");
+      count($result), 1, "Found renamed 'NowDoe' surname");
 
     // Read only John Doe entry.
     $result = DBTNGExampleStorage::load(array('name' => 'John', 'surname' => 'Doe'));
     $this->assertEqual(
-        count($result), 1, 'Found one entry for John Doe.'
+      count($result), 1, 'Found one entry for John Doe.'
     );
     // Get the entry.
     $entry = (array) end($result);
@@ -145,16 +194,16 @@ class DBTNGExampleTest extends WebTestBase {
     // Read only John Doe entry.
     $result = DBTNGExampleStorage::load(array('surname' => 'NowDoe'));
     $this->assertEqual(
-        count($result), 1, 'Found one entry with surname = Nowdoe.'
+      count($result), 1, 'Found one entry with surname = Nowdoe.'
     );
 
     // Verify it is Jane NowDoe.
     $entry = (array) end($result);
     $this->assertEqual(
-        $entry['name'], 'Jane', 'The name Jane is found in the entry'
+      $entry['name'], 'Jane', 'The name Jane is found in the entry'
     );
     $this->assertEqual(
-        $entry['surname'], 'NowDoe', 'The surname NowDoe is found in the entry'
+      $entry['surname'], 'NowDoe', 'The surname NowDoe is found in the entry'
     );
 
     // Delete the entry.
@@ -163,7 +212,7 @@ class DBTNGExampleTest extends WebTestBase {
     // Verify that now there are only 3 records.
     $result = DBTNGExampleStorage::load();
     $this->assertEqual(
-        count($result), 3, 'Found only three records, a record was deleted.'
+      count($result), 3, 'Found only three records, a record was deleted.'
     );
   }
 
