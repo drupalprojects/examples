2868f3719f49b7025550d719807b3a91c560237f
Mile23 <Mile23@116231.no-reply.drupal.org>
2014-07-15 17:51:18 -0700

    Issue #2303923 by Mile23: Convert examples module tests from getInfo() to annotation.

--- src/Tests/ExamplesTest.php
+++ src/Tests/ExamplesTest.php
@@ -12,6 +12,8 @@ use Drupal\simpletest\WebTestBase;
 /**
  * Minimal test case for the examples module.
  *
+ * @group examples
+ *
  * @ingroup examples
  */
 class ExamplesTest extends WebTestBase {
@@ -24,26 +26,10 @@ class ExamplesTest extends WebTestBase {
   public static $modules = array('examples');
 
   /**
-   * Implements getInfo().
-   *
-   * @return array
-   *   Testing info for SimpleTest.
-   */
-  public static function getInfo() {
-    return array(
-      'name' => 'Examples test',
-      'description' => 'Functional tests for the Examples module',
-      'group' => 'Examples',
-    );
-  }
-
-  /**
    * Test whether the module was installed.
-   *
-   * @todo: Replace module_exists with Drupal::moduleHandler() ..etc.
    */
   public function testExamples() {
-    $this->assertTrue(module_exists('examples'));
+    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('examples'));
   }
 
 }
