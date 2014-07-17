5f028615892c4645aa45e3e978bdb4636dc08887
martin107 <martin107@2659379.no-reply.drupal.org>
2014-07-17 15:13:32 -0700

    Issue #2305259 by martin107 | Mile23: Convert js_example tests from getInfo() to annotation.

--- js_example/src/Tests/JsExampleTest.php
+++ js_example/src/Tests/JsExampleTest.php
@@ -10,9 +10,11 @@ namespace Drupal\js_example\Tests;
 use Drupal\simpletest\WebTestBase;
 
 /**
- * Minimal test case for the js_example module.
+ * Functional tests for the js_example module.
  *
  * @ingroup js_example
+ * @group js_example
+ * @group examples
  */
 class JsExampleTest extends WebTestBase {
 
@@ -22,17 +24,6 @@ class JsExampleTest extends WebTestBase {
    * @var array
    */
   public static $modules = array('js_example');
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function getInfo() {
-    return array(
-      'name' => 'JavaScript Examples Tests',
-      'description' => 'Functional tests for the js_example module',
-      'group' => 'Examples',
-    );
-  }
 
   /**
    * Test all the paths defined by our module.
