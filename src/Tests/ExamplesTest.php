ea1dc41b98879f64063156918fc12f4ca1136b61
Mile23 <Mile23@116231.no-reply.drupal.org>
2014-08-13 20:20:38 -0700

    Issue #2318729 by socketwench, Mile23: Added Create a global examples toolbar.

--- src/Tests/ExamplesTest.php
+++ src/Tests/ExamplesTest.php
@@ -23,13 +23,32 @@ class ExamplesTest extends WebTestBase {
    *
    * @var array
    */
-  public static $modules = array('examples');
+  public static $modules = array('examples', 'toolbar');
 
   /**
    * Test whether the module was installed.
    */
   public function testExamples() {
-    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('examples'));
+    // Verify that the toolbar tab and tray are showing and functioning.
+    $user = $this->drupalCreateUser(array('access toolbar'));
+    $this->drupalLogin($user);
+
+    // Check for the 'Examples' tab.
+    $this->drupalGet('/');
+    $this->assertRaw('<nav class="toolbar-lining clearfix" role="navigation" aria-label="Developer Examples">');
+    $this->assertText('Examples');
+    $this->assertNoRaw('<li class="phpunit-example">');
+
+    // Install phpunit_example and see if it appears in the toolbar. We use
+    // phpunit_example because it's very light-weight.
+    $module_handler = $this->container->get('module_handler');
+    $module_handler->install(array('phpunit_example'));
+    // SimpleTest needs for us to reset all the caches.
+    $this->resetAll();
+
+    // Verify that PHPUnit appears in the tray.
+    $this->drupalGet('/');
+    $this->assertRaw('<li class="phpunit-example">');
   }
 
 }
