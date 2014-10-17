ce6e3fabd966b77072607977481f9db8fd3e8c5e
cilefen <cilefen@1850070.no-reply.drupal.org>
2014-10-17 11:24:19 -0700

    Issue #2357439 by cilefen | Mile23: Use service injection for email_example.

--- email_example/src/Form/EmailExampleGetFormPage.php
+++ email_example/src/Form/EmailExampleGetFormPage.php
@@ -7,13 +7,41 @@
 
 namespace Drupal\email_example\Form;
 
-use Drupal\Core\Form\FormInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Mail\MailManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * File test form class.
+ *
+ * @ingroup email_example
  */
-class EmailExampleGetFormPage implements FormInterface {
+class EmailExampleGetFormPage extends FormBase {
+
+  /**
+   * The mail manager.
+   *
+   * @var \Drupal\Core\Mail\MailManagerInterface
+   */
+  protected $mailManager;
+
+  /**
+   * Constructs a new EmailExampleGetFormPage.
+   *
+   * @param \Drupal\Core\Mail\MailManagerInterface $mail_manager
+   *   The mail manager.
+   */
+  public function __construct(MailManagerInterface $mail_manager) {
+    $this->mailManager = $mail_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('plugin.manager.mail'));
+  }
 
   /**
    * Returns a unique string identifying the form.
@@ -62,7 +90,52 @@ class EmailExampleGetFormPage implements FormInterface {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    email_example_mail_send($form_state->getValues());
-  }
+    $form_values = $form_state->getValues();
 
+    // All system mails need to specify the module and template key (mirrored
+    // from hook_mail()) that the message they want to send comes from.
+    $module = 'email_example';
+    $key = 'contact_message';
+
+    // Specify 'to' and 'from' addresses.
+    $to = $form_values['email'];
+    $from = \Drupal::config('system.site')->get('mail');
+
+    // "params" loads in additional context for email content completion in
+    // hook_mail(). In this case, we want to pass in the values the user entered
+    // into the form, which include the message body in $form_values['message'].
+    $params = $form_values;
+
+    // The language of the e-mail. This will one of three values:
+    // - $account->getPreferredLangcode(): Used for sending mail to a particular
+    //   website user, so that the mail appears in their preferred language.
+    // - \Drupal::currentUser()->getPreferredLangcode(): Used when sending a
+    //   mail back to the user currently viewing the site. This will send it in
+    //   the language they're currently using.
+    // - \Drupal::languageManager()->getDefaultLanguage()->getId: Used when
+    //   sending mail to a pre-existing, 'neutral' address, such as the system
+    //   e-mail address, or when you're unsure of the language preferences of
+    //   the intended recipient.
+    //
+    // Since in our case, we are sending a message to a random e-mail address
+    // that is not necessarily tied to a user account, we will use the site's
+    // default language.
+    $language_code = \Drupal::languageManager()->getDefaultLanguage()->getId();
+
+    // Whether or not to automatically send the mail when we call mail() on the
+    // mail manager. This defaults to TRUE, and is normally what you want unless
+    // you need to do additional processing before the mail manager sends the
+    // message.
+    $send_now = TRUE;
+    // Send the mail, and check for success. Note that this does not guarantee
+    // message delivery; only that there were no PHP-related issues encountered
+    // while sending.
+    $result = $this->mailManager->mail($module, $key, $to, $language_code, $params, $from, $send_now);
+    if ($result['result'] == TRUE) {
+      drupal_set_message(t('Your message has been sent.'));
+    }
+    else {
+      drupal_set_message(t('There was a problem sending your message and it was not sent.'), 'error');
+    }
+  }
 }
