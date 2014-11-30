2257343b70d3efd2e2f99cc09a79843fec07b6ae
Paul Mitchum <paul@mile23.com>
2014-11-29 20:46:42 -0600

    Branch Blocker: Changes in the way node deals with config entities.

--- node_type_example/src/Controller/NodeTypeExampleController.php
+++ node_type_example/src/Controller/NodeTypeExampleController.php
@@ -42,10 +42,10 @@ directory. Like this:</p>
 <ul>
 <li>Install Drupal 8.</li>
 <li>Create a new content type at <code>@add_types_url</code>. Let&#39;s call it &#39;Nifty Content Type&#39;.</li>
-<li>Look in <code>sites/default/files/config_[some hex codes]/active/</code>. You&#39;ll see a file called <code>node.type.nifty_content_type.yml</code>.</li>
-<li>Copy or move that file to your module&#39;s <code>config/</code> directory.</li>
+<li>Export the configuration from <code>admin/config/development/configuration</code>. Specific steps depending on needs, and decisions made during Drupal 8 beta. You&#39;ll see a file called <code>node.type.nifty_content_type.yml</code>.</li>
+<li>Copy or move that file to your module&#39;s <code>config/install</code> directory, along with associated field and form yml files.</li>
 </ul>
-<p>You can see two of these YAML files in this module&#39;s config/ directory.</p>
+<p>You can see some of these YAML files in this module&#39;s <code>config/install</code> directory.</p>
 <p>If you want to lock a content type created in this way, you&#39;ll have to implement <code>hook_install()</code> and <code>hook_uninstall()</code>. In <code>hook_install()</code>, you&#39;ll set the content type to be locked. In <code>hook_uninstall()</code> you&#39;ll set the content type to be unlocked.</p>
 <p>Content types created in this way will remain available after the user has uninstalled the module. If you were to fail to set the content type as unlocked, the user would not be able to delete it.</p>
 <p>This example is based on this change notification: <a href="https://drupal.org/node/2029519">https://drupal.org/node/2029519</a></p>',
