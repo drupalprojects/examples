ff6f6157d2536751bcc93e86265bfc61c5ea27be
jeevanbhushetty <jeevanbhushetty@3382246.no-reply.drupal.org>
2017-03-14 18:25:00 -0700

    Issue #2799521 by jeevanbhushetty, navneet0693: Convert BlockExampleController to use DescriptionTemplateTrait

--- block_example/src/Controller/BlockExampleController.php
+++ block_example/src/Controller/BlockExampleController.php
@@ -2,26 +2,18 @@
 
 namespace Drupal\block_example\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Link;
+use Drupal\examples\Utility\DescriptionTemplateTrait;
 /**
  * Controller routines for block example routes.
  */
-class BlockExampleController extends ControllerBase {
+class BlockExampleController {
+  use DescriptionTemplateTrait;
 
   /**
-   * A simple controller method to explain what the block example is about.
+   * {@inheritdoc}
    */
-  public function description() {
-    // Make a link from a route to the block admin page.
-    $block_admin_link = Link::createFromRoute($this->t('the block admin page'), 'block.admin_display')->toString();
-
-    // Put the link into the content.
-    $build = array(
-      '#markup' => $this->t('The Block Example provides three sample blocks which demonstrate the various block APIs. To experiment with the blocks, enable and configure them on @block_admin_link.', ['@block_admin_link' => $block_admin_link]),
-    );
-
-    return $build;
+  protected function getModuleName() {
+    return 'block_example';
   }
 
 }
