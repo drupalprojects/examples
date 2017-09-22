eceaf528de287a026baad1af5bc9818faec5c9c9
Paul Mitchum <paul@mile23.com>
2017-09-22 10:30:46 -0700

    Added easy WTB->BTB conversion test.

---
+++ testing_example/src/Tests/NaiveConversionTest.php
@@ -0,0 +1,24 @@
+<?php
+// @codingStandardsIgnoreFile
+
+// Move this file to testing_example/tests/src/Functional/.
+
+// Change this namespace to Drupal\Tests\testing_example\Functional.
+namespace Drupal\testing_example\Tests;
+
+// Change this to use Drupal\Tests\BrowserTestBase.
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Change this class declaration so it extends BrowserTestBase.
+ */
+class NaiveConversionTest extends WebTestBase {
+
+  /**
+   * A very basic passing test method that requires no further conversion.
+   */
+  public function testConversion() {
+    $this->assertTrue(TRUE);
+  }
+
+}
