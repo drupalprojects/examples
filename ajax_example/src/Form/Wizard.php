6e81c0c7a8fbf206e07250781aa8f04389fcf635
abhilal007 <abhilal007@3473743.no-reply.drupal.org>
2018-07-05 17:18:44 -0600

    Issue #2930038 by jlbellido, Lal_, cosolom, No Sssweat, andeersg, ccshannon, monymirza: AJAX Wizard example is broken

--- ajax_example/src/Form/Wizard.php
+++ ajax_example/src/Form/Wizard.php
@@ -6,8 +6,6 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
 use Drupal\Core\Link;
-use Drupal\Core\Ajax\AjaxResponse;
-use Drupal\Core\Ajax\HtmlCommand;
 
 /**
  * AJAX example wizard.
@@ -44,33 +42,94 @@ class Wizard extends FormBase {
     ];
 
     $form['step'] = [
-      '#type' => 'hidden',
+      '#type' => 'value',
       '#value' => !empty($form_state->getValue('step')) ? $form_state->getValue('step') : 1,
     ];
-    print_r($form_state->getValue('step'));
 
-    if ($form['step']['#value'] == 1) {
-      $form['step1'] = [
-        '#type' => 'fieldset',
-        '#title' => $this->t('Step 1: Personal details'),
-      ];
-      $form['step1']['name'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('Your name'),
-        '#default_value' => empty($form_state->getValue([
-          'step1',
-          'name',
-        ]) ? '' : $form_state->getValue(['step1', 'name'])),
-        '#required' => TRUE,
-      ];
+    switch ($form['step']['#value']) {
+      case 1:
+        $limit_validation_errors = [['step']];
+        $form['step1'] = [
+          '#type' => 'fieldset',
+          '#title' => $this->t('Step 1: Personal details'),
+        ];
+        $form['step1']['name'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('Your name'),
+          '#default_value' => $form_state->hasValue(['step1', 'name']) ? $form_state->getValue(['step1', 'name']) : '',
+          '#required' => TRUE,
+        ];
+        break;
 
-      $form['next'] = [
+      case 2:
+        $limit_validation_errors = [['step'], ['step1']];
+        $form['step1'] = [
+          '#type' => 'value',
+          '#value' => $form_state->getValue('step1'),
+        ];
+        $form['step2'] = [
+          '#type' => 'fieldset',
+          '#title' => t('Step 2: Street address info'),
+        ];
+        $form['step2']['address'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('Your street address'),
+          '#default_value' => $form_state->hasValue(['step2', 'address']) ? $form_state->getValue(['step2', 'address']) : '',
+          '#required' => TRUE,
+        ];
+        break;
+
+      case 3:
+        $limit_validation_errors = [['step'], ['step1'], ['step2']];
+        $form['step1'] = [
+          '#type' => 'value',
+          '#value' => $form_state->getValue('step1'),
+        ];
+        $form['step2'] = [
+          '#type' => 'value',
+          '#value' => $form_state->getValue('step2'),
+        ];
+        $form['step3'] = [
+          '#type' => 'fieldset',
+          '#title' => $this->t('Step 3: City info'),
+        ];
+        $form['step3']['city'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('Your city'),
+          '#default_value' => $form_state->hasValue(['step3', 'city']) ? $form_state->getValue(['step3', 'city']) : '',
+          '#required' => TRUE,
+        ];
+        break;
+    }
+
+    $form['actions'] = ['#type' => 'actions'];
+    if ($form['step']['#value'] > 1) {
+      $form['actions']['prev'] = [
         '#type' => 'submit',
-        '#value' => $this->t('Next step'),
+        '#value' => $this->t('Previous step'),
+        '#limit_validation_errors' => $limit_validation_errors,
+        '#submit' => ['::prevSubmit'],
         '#ajax' => [
-          'wrapper' => 'ajax-example-wizard',
+          'wrapper' => 'ajax-example-wizard-wrapper',
           'callback' => '::prompt',
         ],
+      ];
+    }
+    if ($form['step']['#value'] != 3) {
+      $form['actions']['next'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Next step'),
+        '#submit' => ['::nextSubmit'],
+        '#ajax' => [
+          'wrapper' => 'ajax-example-wizard-wrapper',
+          'callback' => '::prompt',
+        ],
+      ];
+    }
+    if ($form['step']['#value'] == 3) {
+      $form['actions']['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t("Submit your information"),
       ];
     }
 
@@ -82,9 +141,12 @@ class Wizard extends FormBase {
     if ($no_js_use) {
       // Remove the #ajax from the above, so ajax.js won't be loaded.
       // For demonstration only.
-      unset($form['next']['#ajax']);
-      unset($form['prev']['#ajax']);
+      unset($form['actions']['next']['#ajax']);
+      unset($form['actions']['prev']['#ajax']);
     }
+
+    $form['#prefix'] = '<div id="ajax-example-wizard-wrapper">';
+    $form['#suffix'] = '</div>';
 
     return $form;
   }
@@ -105,124 +167,49 @@ class Wizard extends FormBase {
   }
 
   /**
+   * Ajax callback that moves the form to the next step and rebuild the form.
+   *
+   * @param array $form
+   *   The Form API form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The FormState object.
+   *
+   * @return array
+   *   The Form API form.
+   */
+  public function nextSubmit(array $form, FormStateInterface $form_state) {
+    $form_state->setValue('step', $form_state->getValue('step') + 1);
+    $form_state->setRebuild();
+    return $form;
+  }
+
+  /**
+   * Ajax callback that moves the form to the previous step.
+   *
+   * @param array $form
+   *   The Form API form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The FormState object.
+   *
+   * @return array
+   *   The Form API form.
+   */
+  public function prevSubmit(array $form, FormStateInterface $form_state) {
+    $form_state->setValue('step', $form_state->getValue('step') - 1);
+    $form_state->setRebuild();
+    return $form;
+  }
+
+  /**
    * Save away the current information.
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    if ($form_state->getTriggeringElement()['#value'] == $this->t('Submit your information')) {
-      $value_message = $this->t('Your information has been submitted:') . ' ';
-      foreach ($form_state->getValue('value') as $step => $values) {
-        $value_message .= "$step: ";
-        foreach ($values as $key => $value) {
-          $value_message .= "$key=$value, ";
-        }
-      }
-      $this->messenger()->addMessage($value_message);
-      $form_state->setRebuild(FALSE);
-      // Redirect to #action, else return.
-      return;
-    }
-    else {
-      $step = $form_state->getValue('step');
-      // Increment or decrement the step as needed. Recover values if they
-      // exist.
-      if ($form_state->getTriggeringElement()['#value']->__toString() == $this->t('Next step')) {
-        $step++;
-      }
-      elseif ($form_state->getTriggeringElement()['#value']->__toString() == $this->t('Previous step')) {
-        $step--;
-      }
 
-      switch ($step) {
-        case 1:
-          $form['step1'] = [
-            '#type' => 'fieldset',
-            '#title' => $this->t('Step 1: Personal details'),
-          ];
-          $form['step1']['name'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('Your name'),
-            '#default_value' => empty($form_state->getValue([
-              'step1',
-              'name',
-            ]) ? '' : $form_state->getValue(['step1', 'name'])),
-            '#required' => TRUE,
-          ];
-          $form_state->setValue('step', 1);
-          break;
-
-        case 2:
-          unset($form['step1']);
-          unset($form['next']);
-          $form['step2'] = [
-            '#type' => 'fieldset',
-            '#title' => $this->t('Step 2: Street address info'),
-          ];
-          $form['step2']['address'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('Your street address'),
-            '#default_value' => empty($form_state->getValue([
-              'step2',
-              'address',
-            ]) ? '' : $form_state->getValue(['step2', 'address'])),
-            '#required' => TRUE,
-          ];
-          $form_state->setValue('step', $step);
-          break;
-
-        case 3:
-
-          $form['step3'] = [
-            '#type' => 'fieldset',
-            '#title' => $this->t('Step 3: City info'),
-          ];
-          $form['step3']['city'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('Your city'),
-            '#default_value' => empty($form_state->getValue([
-              'step3',
-              'city',
-            ]) ? '' : $form_state->getValue(['step3', 'city'])),
-            '#required' => TRUE,
-          ];
-          $form_state->setValue('step', $step);
-          break;
-      }
-      if ($step == 3) {
-
-        $form['submit'] = [
-          '#type' => 'submit',
-          '#value' => $this->t("Submit your information"),
-        ];
-      }
-      if ($step > 1 && !isset($form['prev'])) {
-        $form['prev'] = [
-          '#type' => 'submit',
-          '#value' => $this->t("Previous step"),
-          // Since all info will be discarded, don't validate on 'prev'.
-          '#limit_validation_errors' => [],
-          // #submit is required to use #limit_validation_errors.
-          '#submit' => ['ajax_example_wizard_submit'],
-          '#ajax' => [
-            'wrapper' => 'ajax-example-wizard',
-            'callback' => '::prompt',
-          ],
-        ];
-      }
-      if ($step < 3 && !isset($form['next'])) {
-        $form['next'] = [
-          '#type' => 'submit',
-          '#value' => $this->t('Next step'),
-          '#limit_validation_errors' => [],
-          '#ajax' => [
-            'wrapper' => 'ajax-example-wizard',
-            'callback' => '::prompt',
-          ],
-        ];
-      }
-      $response = new AjaxResponse();
-      $response->addCommand(new HtmlCommand('#ajax-example-wizard', $form));
-      return $response;
-    }
+    $messenger = \Drupal::messenger();
+    $messenger->addMessage($this->t('Your information has been submitted:'));
+    $messenger->addMessage($this->t('Name: @name', ['@name' => $form_state->getValue(['step1', 'name'])]));
+    $messenger->addMessage($this->t('Address: @address', ['@address' => $form_state->getValue(['step2', 'address'])]));
+    $messenger->addMessage($this->t('City: @city', ['@city' => $form_state->getValue(['step3', 'city'])]));
 
   }
 
